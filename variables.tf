8be2abb0cce80db8b761984c2e10de5e0536b7f6
Andriy Knysh <aknysh@users.noreply.github.com>
2019-01-06 18:39:35 -0800

    Update variables.tf
    Co-Authored-By: osterman <erik@cloudposse.com>

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "name" {
 variable "zone_id" {
   type        = "string"
   default     = ""
-  description = "Route53 parent zone ID. If provided (not empty), the module will create sub-domain DNS records for the DB master and replicas"
+  description = "Route53 parent zone ID. If provided (not empty), the module will create sub-domain DNS record for the cluster endpoint"
 }
 
 variable "security_groups" {
